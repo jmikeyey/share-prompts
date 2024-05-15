f557355e953fa99391d50f9805ab73f84576db9e
Mayki <aw8hurtz@gmail.com>
2024-05-15 13:11:16 +0800

    nav layout and homepages

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,6 @@
 import "@styles/globals.css";
+import Nav from "@components/Nav";
+import Provider from "@components/Provider";
 
 export const metadate = {
   title: "Promptopia",
@@ -12,7 +14,10 @@ const RootLayout = ({ children }: { children: React.ReactNode }) => {
         <div className="main">
           <div className="gradient" />
         </div>
-        <main className="app">{children}</main>
+        <main className="app">
+          <Nav />
+          {children}
+        </main>
       </body>
     </html>
   );
--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,23 @@
+import Feed from "@components/Feed";
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <section className="w-full flex-center flex-col">
+      <h1 className="head_text text-center">
+        Discover & Share
+        <br className="max-md:hidden" />
+        <span className="orange_gradient text-center">
+          {" "}
+          AI-Powered Prompts{" "}
+        </span>
+      </h1>
+      <p className="desc text-center">
+        Share-Prompt is an open-source AI propmpting tool for modern world to
+        discover, create and share createive prompts
+      </p>
+
+      <Feed />
+    </section>
+  );
 };
 
 export default Home;
